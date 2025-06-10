55938efdba33789e58a4446a9739e4800927e3c6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-10 20:06:14 +0300

    Create part-z-0003-basic-types2.sol

---
+++ part-z-0003-basic-types2.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    uint8 public age = 17;
+
+    uint16 public year = 2016;
+
+    uint32 public sales = 120_300;
+
+    // 64... 256
+
+    uint256 public budget = 2_521_612_439;
+
+    uint public amount = 521_612_439_874;
+
+}
