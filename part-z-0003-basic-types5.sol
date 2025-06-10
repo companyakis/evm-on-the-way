95f3fd27a2c5195e726153e79d30172dda012d06
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-10 20:16:40 +0300

    Create part-z-0003-basic-types5.sol

---
+++ part-z-0003-basic-types5.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    address public addr = 0x6d2e03b7EfFEae98BD302A9F836D0d6Ab0002766;
+
+    address public defaultAddress; // 0x0000000000000000000000000000000000000000
+
+}
