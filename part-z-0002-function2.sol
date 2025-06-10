75c94a463ed03b15bacc86154eea7dba95e0814d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-10 14:00:48 +0300

    Create part-z-0002-function2.sol

---
+++ part-z-0002-function2.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    function sum(uint x, uint y) external pure returns (uint) {
+
+        return x + y;
+    }
+
+    function mult(uint x, uint y) external pure returns (uint) {
+
+        return x * y;
+    }
+
+}
