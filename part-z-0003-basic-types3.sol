503d5c1886582b3fe9ea55a6c6c548b36c51c258
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-10 20:09:19 +0300

    Create part-z-0003-basic-types3.sol

---
+++ part-z-0003-basic-types3.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    // from 8 to 256
+    // positive or negative values, also zero...
+
+    int8 public age = 17;
+
+    int16 public val = -2016;
+
+    int32 public sales = 0;
+
+    int256 public loss = -421_521_612_439;
+
+    int public totalLost = -2_521_612_439_874;
+
+}
