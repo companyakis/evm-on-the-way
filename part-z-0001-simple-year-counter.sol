695894497c4de9a475fc38676e9ba09f86c98d53
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-10 13:33:33 +0300

    Update part-z-0001-simple-year-counter.sol

--- part-z-0001-simple-year-counter.sol
+++ part-z-0001-simple-year-counter.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    uint16 public year;
+
+    constructor(uint16 _y) {
+
+        year = _y;
+    }
+
+    function yearUp() public {
+
+        year += 1;
+    }
+
+    function yearDown() public {
+
+        require(year > 2020, "Old years! We are here!");
+        
+        year -= 1;
+    }
+
+}
 
