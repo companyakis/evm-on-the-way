3a452261824061fc3018375c05ef4bc0b983a812
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-10 20:02:08 +0300

    Create part-z-0003-basic-types1.sol

---
+++ part-z-0003-basic-types1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    bool public isReady = false;
+
+    bool public completed = true;
+
+    bool public condition = !isReady;
+
+}
