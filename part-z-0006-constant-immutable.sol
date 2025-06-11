24c1cbb70576e33fdae61641a75ac60bd3e5adef
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-11 18:03:28 +0300

    Update part-z-0006-constant-immutable.sol

--- part-z-0006-constant-immutable.sol
+++ part-z-0006-constant-immutable.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    // hard coded and using constants can save gas cost
+
+    uint16 public constant MAX_AMOUNT = 15_500;
+
+    string public constant FOUNDER = "Mustafa Buyukdereli";
+
+    // can be set inside the constructor but cannot be modified afterwards
+
+    uint16 public immutable birthYear;
+
+    constructor(uint16 _by) {
+
+        birthYear = _by;
+    }
+
+}
 
