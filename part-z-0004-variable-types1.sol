0e8fbcc400a1f880e79db9aba69e8ed90be2f191
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-11 17:15:41 +0300

    Create part-z-0004-variable-types1.sol

---
+++ part-z-0004-variable-types1.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    // state variables
+
+    bool public isOk = true;
+
+    uint16 public year = 2025;
+
+    string public name = "Mustafa Buyukdereli";
+
+}
