48eca51ea63a4d22e3a1da5a1467b34589087e8f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-11 17:48:21 +0300

    Create part-z-0004-variable-types3.sol

---
+++ part-z-0004-variable-types3.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    // global variables
+
+    uint256 public finalTime = block.timestamp + 2 days;
+
+    address public contractOwner = msg.sender;
+
+}
