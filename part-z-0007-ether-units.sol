3c9d9158ab4ae04568dc2d07b8b0e6ab12791ed5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-11 18:10:25 +0300

    Create part-z-0007-ether-units.sol

---
+++ part-z-0007-ether-units.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    uint public threeGwei = 3 * 1 gwei;
+
+    // 1 gwei == 10 ** 9 wei
+
+    bool public isEqual = 1 gwei == 10 ** 9 wei;
+
+    // 1 ether == 10 ** 18 wei
+
+    bool public isTrue = 2 ether == 2 * 10 ** 18 wei;
+
+}
