99b17d77c0f33693cf848808558301a4776d1d99
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-16 11:41:05 +0300

    Create part-z-0012-enum1.sol

---
+++ part-z-0012-enum1.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    enum Months {
+        January, // 0
+        February,
+        March,
+        April,
+        May,
+        June
+    }
+
+    Months public firstMonth = Months.January;
+
+}
