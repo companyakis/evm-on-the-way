8903807ad975217c30234c5a624d27980a0e4b4a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-16 10:09:58 +0300

    Create part-z-0502-solidity-functions2.sol

---
+++ part-z-0502-solidity-functions2.sol
@@ -0,0 +1,27 @@
+/*
+Your Goal: Increment x
+Let's build on your code from the previous stage!
+
+Create an external function called increment that will add 1 to the state variable x.
+
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    uint public x;
+
+    constructor(uint _num) {
+
+        x = _num;
+    }
+
+
+    function increment() external {
+
+        x += 1;
+    }
+
+}
