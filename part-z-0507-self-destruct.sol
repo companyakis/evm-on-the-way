0a8b5edda18bbcb4690c77257b450823ecf046bf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-18 10:40:49 +0300

    Create part-z-0507-self-destruct.sol

---
+++ part-z-0507-self-destruct.sol
@@ -0,0 +1,7 @@
+/*
+Your Goal: Self Destruct
+When the donate function is called, trigger a selfdestruct in the contract!
+
+The selfdestruct will send all remaining funds to the address passed in, so it might be a good candidate to replace the existing functionality in your donate function by sending the funds to the charity! 
+*/
+
