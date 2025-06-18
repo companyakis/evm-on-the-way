5dbbf3c00d28613941def61c3b4c115efd516a58
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-18 11:15:12 +0300

    Create part-z-0508-constructor-revert.sol

---
+++ part-z-0508-constructor-revert.sol
@@ -0,0 +1,8 @@
+/*
+Your Goal: Require 1 Ether
+Add a payable constructor method that requires a 1 ether deposit.
+
+If at least 1 ether is not sent to the constructor, revert the transaction.
+
+*/
+
