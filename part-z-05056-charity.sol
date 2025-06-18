02be293544157e6eb392d351bd9cbd1feb261654
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-06-18 10:18:40 +0300

    Create part-z-05056-charity.sol

---
+++ part-z-05056-charity.sol
@@ -0,0 +1,9 @@
+/*
+ Your Goal: Charity Donation
+Let's take all funds that were passed to the receive function and donate them to charity. We'll do this in two steps.
+
+First, modify the constructor to accept a new argument: the charity address.
+Next, add a new function called donate. When this function is called transfer all remaining funds in the contract to the charity address.
+
+*/
+
