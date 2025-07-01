3e55f415717bc088a6ff8a921220b2420ec6f42b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-01 10:22:48 +0300

    Create part-z-0806-arrays4.sol

---
+++ part-z-0806-arrays4.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Arrays {
+
+// Inside a function named 'removeElement', remove the last element from the 'dynamicArray'
+
+    uint256[] public dynamicArray;
+    
+    function removeElement() public {
+
+        dynamicArray.pop();
+    }
+}
