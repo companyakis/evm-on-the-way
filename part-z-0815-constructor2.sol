c914295d3789190ded17404927f2006099cd8a73
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-02 11:22:11 +0300

    Create part-z-0815-constructor2.sol

---
+++ part-z-0815-constructor2.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Constructor {
+
+// Declare a constructor that initializes a uint variable 'x' with the value 5
+
+    uint x;
+
+    constructor() {
+
+        x = 5;
+    }
+}
