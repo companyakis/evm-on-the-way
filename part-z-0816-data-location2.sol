0eeaf7eaba55306619d1ee5b702a669d789cdc27
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-02 11:33:50 +0300

    Create part-z-0816-data-location2.sol

---
+++ part-z-0816-data-location2.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract DataLocation {
+
+// Declare a function named 'memoryFunction' that declares a memory variable of type uint and name it 'memoryVar'
+
+    function memoryFunction() public pure {
+
+        uint memoryVar;
+    }
+}
