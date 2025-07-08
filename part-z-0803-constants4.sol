638af6c0c5284ce3450f03b8e4bdc335fc1f3fd0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-08 20:32:49 +0300

    Delete part-z-0803-constants4.sol

--- part-z-0803-constants4.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.24;
-
-contract Constants {
-
-// Declare a constant of type string with the name 'CONTRACT_VERSION' and initialize it with the value 'v1.0.0'
-
-    string constant CONTRACT_VERSION = 'v1.0.0';
-}
