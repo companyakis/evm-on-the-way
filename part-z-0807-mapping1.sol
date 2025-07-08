2f75411e77e93312b7069e205e7eb698e9a39d1e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-08 20:36:34 +0300

    Delete part-z-0807-mapping1.sol

--- part-z-0807-mapping1.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.24;
-
-contract Mapping {
-
-// Declare a mapping from address to uint256 with the name 'myMap'
-
-    mapping(address => uint256) public myMap;
-}
