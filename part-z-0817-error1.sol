e5bf0a871480cde30ff20e0170d577be0d01a6c8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-08 20:47:16 +0300

    Delete part-z-0817-error1.sol

--- part-z-0817-error1.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.24;
-
-contract Errors {
-
-// Declare an error named 'InsufficientFunds' with a string parameter 'needed'
-
-    error InsufficientFunds(string needed);
-}
