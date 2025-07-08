c6661759d4356a9cd476f6be0c9c452de2ac6b22
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-08 20:40:10 +0300

    Delete part-z-0810-getter-functions1.sol

--- part-z-0810-getter-functions1.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.24;
-
-contract GetterFunctions {
-
-// Declare a public state variable of type uint named 'stateVar' and initialize it with the value 10
-
-    uint public stateVar = 10;
-}
