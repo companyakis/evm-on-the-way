74fb0cdaea265f7d31ce448dd740abf3c8585fa4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-08 20:35:59 +0300

    Delete part-z-0806-arrays2.sol

--- part-z-0806-arrays2.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.24;
-
-contract Arrays {
-
-// Declare a dynamic size array of type uint256 with the name 'dynamicArray'
-
-    uint256[] public dynamicArray;
-}
