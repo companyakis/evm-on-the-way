e80f995c408f1a1538b0fd5acc905cab55fbcb5c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-10 20:08:33 +0300

    Create part-y-0035-data-location3.sol

---
+++ part-y-0035-data-location3.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract DataLocation {
+
+// Declare an external function named 'calldataFunction' that takes a calldata uint argument named 'calldataVar'
+
+    function calldataFunction(uint calldataVar) external pure {
+        
+    }
+}
