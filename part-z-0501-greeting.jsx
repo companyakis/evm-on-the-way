8506dd0c0b99e61628a9a9b773ca72633bfe9230
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-11 01:37:47 +0300

    Create part-z-0501-greeting.jsx

---
+++ part-z-0501-greeting.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hi there! Mustafa Buyukdereli was here...</h1>
+
+      <p>Daaps are loading!..</p>
+
+      <p>My proverb: "A rolling stone gathers no moss!"</p>
+    </>
+  )
+}
+
+export default App
