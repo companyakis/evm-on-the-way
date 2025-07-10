fda11c278d37b764cdc63b74df36836009e52fe3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-10 20:09:58 +0300

    Create part-y-0035-data-location4.sol

---
+++ part-y-0035-data-location4.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract DataLocation {
+
+// Declare a state variable of type uint and name it 'storageVar'
+// Declare a function named 'memoryFunction' that declares a memory variable of type uint and name it 'memoryVar'
+
+    uint storageVar;
+
+    function memoryFunction() public pure {
+
+        uint memoryVar;
+    }
+}
