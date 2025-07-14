29e1d0cae9e02c628e8e80d32e04b4df5d91fb43
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-14 12:05:18 +0300

    Create part-x-0003-assert2.sol

---
+++ part-x-0003-assert2.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "./calculus.sol";
+
+contract Mustafa {
+
+    function profitorLossCalculator(uint256 _sales, uint256 _cost) public pure returns (uint256) {
+
+        assert(_sales != 0);
+
+        assert(_cost > 0 && _cost < 50_000);
+
+        return _sales - _cost;
+    }
+
+}
