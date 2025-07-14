9d33112333872b219fc7be3328b407a6a97200a6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-14 12:42:50 +0300

    Update part-x-0005-modifier1.sol

--- part-x-0005-modifier1.sol
+++ part-x-0005-modifier1.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+contract Mustafa {
+
+    address public teller;
+
+    string public tellerProverb;
+
+    constructor() {
+
+        teller = msg.sender;
+    }
+
+    modifier onlyTeller() {
+
+        require(teller == msg.sender, "Only contract caller can tell a proverb!");
+
+        _;
+    }
+
+    function tellAProverb(string memory _proverb) public onlyTeller {
+
+        tellerProverb = _proverb;
+    }
+}
