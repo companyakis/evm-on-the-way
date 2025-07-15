6c233ab4a6d8ef9e70fc0e53bb1614dfbcde488d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-15 21:42:09 +0300

    Create part-z-0602-basic-syntax.sol

---
+++ part-z-0602-basic-syntax.sol
@@ -0,0 +1,17 @@
+/*
+ Assignment
+Delete the HelloWorld contract and its content.
+Create a new contract named "MyContract".
+The contract should have a public state variable called "name" of the type string.
+Assign the value "Alice" to your new variable.
+
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+contract MyContract {
+
+    string public name = "Alice";
+}
