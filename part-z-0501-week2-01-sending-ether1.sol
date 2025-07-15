19086ccc2b242a06fedf7633d9c76cc8dcf070c4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-15 21:29:49 +0300

    Create part-z-0501-week2-01-sending-ether1.sol

---
+++ part-z-0501-week2-01-sending-ether1.sol
@@ -0,0 +1,18 @@
+/*
+ Your Goal: Store the Owner
+Create a public address state variable called owner on the contract
+Next create a constructor function which will store the msg.sender in owner
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    
+    address public owner;
+
+    constructor() {
+
+        owner = msg.sender;
+    }
+}
