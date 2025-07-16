3987cea569202a22937dda0b291ca08178d6e62e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-16 09:00:48 +0300

    Create part-z-0605-functions4.sol

---
+++ part-z-0605-functions4.sol
@@ -0,0 +1,17 @@
+/*
+⭐️ Assignment
+Create a new function called returnTwo that returns the values -2 and true
+
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract Function {
+
+    function returnTwo() public pure returns (int8, bool) {
+
+        return (-2, true);
+    }
+  
+}
