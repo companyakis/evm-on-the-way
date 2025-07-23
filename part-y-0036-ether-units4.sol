79fb363cf51b2ace3446537541cedb1ce288b845
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-23 23:16:59 +0300

    Create part-y-0036-ether-units4.sol

---
+++ part-y-0036-ether-units4.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract EtherUnits {
+
+// Declare a variable of type uint with the name 'weiToGwei' and convert 1 ether to gwei
+
+    uint weiToGwei = 1 ether / 1 gwei;
+}
