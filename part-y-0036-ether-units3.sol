002898891d019426c1b5f54d1de4b56e505e2f55
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-23 23:13:18 +0300

    Create part-y-0036-ether-units3.sol

---
+++ part-y-0036-ether-units3.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract EtherUnits {
+
+// Declare a variable of type uint with the name 'etherValue' and initialize it with the value of 1 ether
+
+    uint etherValue = 1 ether;
+}
