474f7763bd205b0510f6e67770bc40aed6796d44
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-23 23:10:51 +0300

    Create part-y-0036-ether-units1.sol

---
+++ part-y-0036-ether-units1.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract EtherUnits {
+
+// Declare a variable of type uint with the name 'weiValue' and initialize it with the value of 1 wei
+
+    uint weiValue = 1 wei;
+}
