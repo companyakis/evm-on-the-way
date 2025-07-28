4f6d49ded69af13d8fc365756a61fe41773d7674
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 11:00:27 +0300

    Create part-y-0040-errors1.sol

---
+++ part-y-0040-errors1.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Errors {
+
+// Declare an error named 'InsufficientFunds' with a string parameter 'needed'
+
+    error InsufficientFunds(string needed); 
+}
