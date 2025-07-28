395e5715e9332b1c7c8780c0c6ee7da444eb3fd0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 09:57:01 +0300

    Create part-y-0039-payable5.sol

---
+++ part-y-0039-payable5.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Payable {
+
+// Declare a payable address with the name 'recipient'
+// Initialize 'recipient' with the address of the contract deployer in the constructor
+// Create a payable function named 'deposit' that can receive Ether
+// Create a function named 'withdraw' that sends all Ether stored in the contract to the 'recipient' address
+// Create a receive function that calls the 'deposit' function when the contract receives Ether
+
+    address payable recipient;
+
+    constructor() {
+
+        recipient = payable(msg.sender);
+    }
+
+    function deposit() public payable {
+        
+    }
+
+    function withdraw() public {
+
+        (bool success, ) = recipient.call{value: address(this).balance}('');
+
+        require(success, 'Failed to send Ether');
+    }
+
+    receive() external payable {
+
+        deposit();
+    }
+}
