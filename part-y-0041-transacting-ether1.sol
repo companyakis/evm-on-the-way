72b055cd1bbe6536983598cfc661acb8cd8cd8e4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-04 12:15:36 +0300

    Create part-y-0041-transacting-ether1.sol

---
+++ part-y-0041-transacting-ether1.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract TransactingEther {
+
+// Declare a payable address with the name 'recipient' and initialize it with the Ethereum address 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+
+    address payable recipient = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+}
