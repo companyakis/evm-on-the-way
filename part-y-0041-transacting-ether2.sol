ceb0c95bd85dbdfe0d94decf9d24023b741f2920
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-04 12:17:19 +0300

    Create part-y-0041-transacting-ether2.sol

---
+++ part-y-0041-transacting-ether2.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract TransactingEther {
+
+// Declare a uint256 variable named 'amount' and initialize it with the value 1 ether
+
+    uint256 amount = 1 ether;
+}
