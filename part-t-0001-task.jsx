896e94b484737ea6075484af53d76c994924d43c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-07 00:09:12 +0300

    Create part-t-0001-task.jsx

---
+++ part-t-0001-task.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  
+  let founder = "Mustafa Buyukdereli"
+
+  let year = 2025
+
+  let purpose = "DeFi and Daap creation"
+
+  return (
+    <>
+
+      <div>{founder} was here in {year}...</div>
+
+      <div>Main task is "{purpose}"</div>
+
+    </>
+  );
+}
+
+export default App;
