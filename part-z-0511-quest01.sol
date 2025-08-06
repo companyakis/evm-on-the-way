75339ff5025909eaf3843a3c3686e52726f858f8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-06 21:59:06 +0300

    Create part-z-0511-quest01.sol

---
+++ part-z-0511-quest01.sol
@@ -0,0 +1,8 @@
+/*
+Complete the 'HelloWorld' contract by following these steps:
+
+Declare a state variable of type string named greeting.
+Implement a constructor that sets greeting to "Hello, World!".
+
+*/
+
