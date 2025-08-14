52a61c05b523d98858120417331f3101d0af9273
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-14 11:58:13 +0300

    Create part-s-0007-inheritance3-toolkit.sol

---
+++ part-s-0007-inheritance3-toolkit.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SomeMath {
+
+    function sum(uint x, uint y, uint z) public virtual pure returns (uint) {
+
+        return  x + y + z;
+    }
+}
+
+contract Greeting {
+
+
+}
